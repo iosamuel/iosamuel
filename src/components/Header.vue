c52d7bd9cad6b3db760e1c829e4ef45d16838341
Samuel BR <samuelb1311@gmail.com>
2020-05-18 21:00:48 -0500

    Header fixes & Home Page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,23 @@
 <template>
-  <div>
-    <header>
-      <figure>
-        <img src="@/assets/images/IOS-Isotipo.png" alt="Logo" />
-        <figcaption>
-          <span>IO</span>
-          <span>Samuel</span>
-        </figcaption>
-      </figure>
-      <nav>
-        <button class="nav-button active">
-          <span>Home</span>
-        </button>
-        <button class="nav-button">
-          <span>About</span>
-        </button>
-        <button class="nav-button">
-          <span>Contact</span>
-        </button>
-        <button
-          class="burger"
-          :class="{ active: showNavContent }"
-          @click="showNavContent = !showNavContent"
-        >
-          <fa :icon="['fas', 'bars']"></fa>
-        </button>
-      </nav>
-    </header>
-    <section class="nav-content" v-show="showNavContent">
-      <button class="nav-content__button active">Home</button>
-      <button class="nav-content__button">About</button>
-      <button class="nav-content__button">Contact</button>
-    </section>
-  </div>
+  <header>
+    <figure>
+      <img src="@/assets/images/IOS-Isotipo.png" alt="Logo" />
+      <figcaption>
+        <span>IO</span>
+        <span>Samuel</span>
+      </figcaption>
+    </figure>
+    <nav :class="{ 'show-nav': showNavContent }">
+      <section class="nav-content">
+        <button class="nav-content__button active"><span>Home</span></button>
+        <button class="nav-content__button"><span>About</span></button>
+        <button class="nav-content__button"><span>Contact</span></button>
+      </section>
+      <button class="burger" @click="showNavContent = !showNavContent">
+        <fa :icon="['fas', 'bars']"></fa>
+      </button>
+    </nav>
+  </header>
 </template>
 
 <script>
--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -2,8 +2,25 @@ import Vue from "vue";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBars, faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faGithub,
+  faTwitch,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faBars, faArrowCircleUp);
+library.add(
+  faBars,
+  faArrowCircleUp,
+  faTwitter,
+  faFacebook,
+  faLinkedin,
+  faGithub,
+  faTwitch,
+  faYoutube
+);
 
 Vue.component("fa", FontAwesomeIcon);
